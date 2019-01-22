0cd6edabb86cd28d732bd02a5b6d313ba29b904e
chris <kosik.chris.j@gmail.com>
2019-01-22 17:30:48 -0500

    Update #2. UI update. Beginning of timer & progress. Changing directories.

--- app/src/main/java/j/chris/kosik/dualmotorcontrol/MainActivity.java
+++ app/src/main/java/j/chris/kosik/dualmotorcontrol/MainActivity.java
@@ -7,6 +7,7 @@ import android.bluetooth.BluetoothSocket;
 import android.content.Context;
 import android.content.Intent;
 import android.os.AsyncTask;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -15,15 +16,24 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.io.IOException;
+import java.util.Timer;
 import java.util.UUID;
 
 import j.chris.kosik.dualmotorcontrol.Utils.DialogsUtils;
 import j.chris.kosik.dualmotorcontrol.Utils.StringUtils;
 
 public class MainActivity extends AppCompatActivity {
+
+    private int progressStatus = 0;
+    public double mmPerSecMove = 0.55;
+    double totalTime;
+    private Handler handler = new Handler();
+    Timer timer = new Timer();
 
     Button stopBtn,discBtn,cycleBtn;
 
@@ -58,7 +68,8 @@ public class MainActivity extends AppCompatActivity {
         final EditText distanceInput = findViewById(R.id.numericTxtInput);
         final EditText timeInput = findViewById(R.id.timeTxtInput);
         final EditText cycleInput = findViewById(R.id.numdutycyclesTxtInput);
-
+        final TextView numCycleLeftText = findViewById(R.id.cyclesRemainingTxt);
+        final ProgressBar cycleProgress = findViewById(R.id.pb);
 
         stopBtn = findViewById(R.id.StopBtn);
         cycleBtn = findViewById(R.id.cycleBtn);
@@ -76,10 +87,42 @@ public class MainActivity extends AppCompatActivity {
         cycleBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                final int numCycles = Integer.parseInt(numCycleLeftText.getText().toString());
                 String distance = appendString.appendDistance(distanceInput.getText().toString(),mainContext,toast);
                 String time = appendString.appendDistance(timeInput.getText().toString(),mainContext,toast);
                 String cycle = appendString.appendCycle(cycleInput.getText().toString(),mainContext,toast);
                 cycleSignal(distance, time, cycle);
+
+                //Create new thread to update the cycles!
+                new Thread(new Runnable() {
+
+
+                    @Override
+                    public void run() {
+                        while(progressStatus < 100){
+                            progressStatus +=1;
+
+
+                            try{
+                                //multiply by 1000 to get milli seconds from user input - just pause time
+                                //TODO: Figure out rate of movement and * by distance to get travel time
+                                Thread.sleep(getCycleTime(Integer.parseInt(timeInput.getText().toString()),distanceInput));
+                            }catch(InterruptedException e){
+                                e.printStackTrace();
+                            }
+
+                            // Update the progress bar
+                            handler.post(new Runnable() {
+                                @Override
+                                public void run() {
+                                    cycleProgress.setProgress(progressStatus);
+                                    // Show the progress on TextView
+                                    numCycleLeftText.setText(numCycles);
+                                }
+                            });
+                        }
+                    }
+                });
             }
         });
         stopBtn.setOnClickListener(new View.OnClickListener() {
@@ -116,6 +159,27 @@ public class MainActivity extends AppCompatActivity {
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    public long getCycleTime(long timeInput, EditText distanceInput){
+        //Receives a decimal of time (seconds)
+        //mult by 1000 to get millisec = pause time in milliseconds
+        //then need travel time
+        long pauseTimeMillisec = timeInput*1000;
+        //At time of this method call distance will be input
+        long distanceTime = (Integer.parseInt(distanceInput.getText().toString())*(long)mmPerSecMove);
+        long cycleTime = pauseTimeMillisec+(2*distanceTime);
+
+        return cycleTime;
+    }
+
+    public double TotalTime(long timeInput, EditText distanceInput, int numCycles){
+        totalTime = 0;
+        //convert to milliseconds first
+        double cycleTime = getCycleTime(timeInput,distanceInput);
+        totalTime = cycleTime*numCycles;
+
+        return totalTime;
     }
 
 
@@ -237,6 +301,7 @@ public class MainActivity extends AppCompatActivity {
             } catch (IOException e) {
                 dialog.msg(mainContext,"Error in sending cycle Signal.");
             }
+
         }
     }
 }
